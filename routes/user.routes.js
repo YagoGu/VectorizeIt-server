bd27808011dff36381578952e85a4a9bd9ebb120
YagoGu <yago.gutierrez15@gmail.com>
2023-08-30 17:45:27 +0200

    new file routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../models/User.model');
+// const User = require('../models/User.model');
 const Videogame = require('../models/Videogame.model');
 
 
