5afea2cbf7168c90d0ba6f7e9677f8c4aeb8c7b5
YagoGu <yago.gutierrez15@gmail.com>
2023-08-31 12:13:27 +0200

    display, update and delete

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,49 @@
 const express = require("express");
 const router = express.Router();
-// const User = require('../models/User.model');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const Videogame = require('../models/Videogame.model');
+const User = require('../models/User.model');
+
+//get your user info
+router.get("/:idUser", (req, res, next) => {
+
+    const {idUser} = req.params;
+
+    User.findById(idUser)
+    .then( (user) => {
+        res.send(user)
+    })
+})
+
+//update your user info
+router.post("/:idUser/update", (req, res, next) => {
+    const {idUser} = req.params;
+    // const {password} = req.body
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync("password", salt);
+
+    User.findByIdAndUpdate(idUser, 
+        {
+            username: "pepitodelospalotes",
+            email: "pepito@gmail.com",
+            birthday: "0006-05-12",
+            profile_picture: "https://www.cnet.com/a/img/resize/61c44c6765cb6b8529df884935ad7aefc622aeec/hub/2021/11/03/3c2a7d79-770e-4cfa-9847-66b3901fb5d7/c09.jpg?auto=webp&fit=crop&height=675&width=1200",
+            password: hashedPassword
+        })
+    .then( (user) => {
+        res.send(user)
+    })
+})
+
+//delete user
+router.post("/:idUser/delete", (req, res, next) => {
+    
+    const {idUser} = req.params;
+
+    User.findByIdAndDelete(idUser)
+    .then ((user) => console.log(user))
+})
 
 // create a new game for the db
 router.post("/:idUser/create", (req, res, next) => {
